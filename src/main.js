523b24cf1c18152ef139469b0527d169165edb5e
abbie <supermercadoabbie@gmail.com>
2023-09-08 15:29:46 +0800

    -- style: installed and configured bootstrap + fontawsome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +8,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('fa-icon', FontAwesomeIcon)
 
 app.mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
